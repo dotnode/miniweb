b8b0f2db8b8f0f52694d4115d736e04bca91718d
xnode <xnode@qq.com>
2024-10-09 14:33:00 +0800

    realpath

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
+(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(realpath(__DIR__)))->bootstrap();
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
@@ -44,7 +42,7 @@ $app->singleton(
 );
 
 if (php_sapi_name() == 'cli') {
-	//cli 加载命令行
+    //cli 加载命令行
     $app->singleton(
         Illuminate\Contracts\Console\Kernel::class,
         App\Console\Kernel::class
